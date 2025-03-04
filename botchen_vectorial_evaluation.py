368763c8d56ba51eb3571fcb7947b76f04f56928
memonji <emma.montecchiari@studenti.unitn.it>
2025-03-04 18:57:57 +0100

    Update evaluation

--- botchen_vectorial_evaluation.py
+++ botchen_vectorial_evaluation.py
@@ -124,14 +124,14 @@ def create_matrices(script_id,
         activation_matrix.append(row)
     df = pd.DataFrame(activation_matrix, columns=all_properties, index=entities)
 
-    if optimal_matrix == 1:
-        print('Optimal matrix has been created')
-    if optimal_matrix == 0:
-        print('Matrix from Botchen conversation has been created')
-    if script_id != 0:
-        print('Matrix from the specific situation has been created')
-    if script_id == 0:
-        print('Matrix from the full corpus has been created')
+    # if optimal_matrix == 1:
+    #     print('Optimal matrix has been created')
+    # if optimal_matrix == 0:
+    #     print('Matrix from Botchen conversation has been created')
+    # if script_id != 0:
+    #     print('Matrix from the specific situation has been created')
+    # if script_id == 0:
+    #     print('Matrix from the full corpus has been created')
 
     if store_space == 1: # Store the matrix?
         df.to_csv(saving_directory, index=True)
@@ -266,99 +266,123 @@ def evaluation_with_vectorial_space(module, language, topk,
                                            entity_properties,
                                            0) # don't store the space
     # Create the matrix from the training data (optimal)
-    optimal_matrix, optimal_df = create_matrices(script_id_optimal,
-                                                 1,
-                                                 entity_properties, 0)
+    for script_id_optimal in range(0, 13):
+        print('Optimal script', script_id_optimal)
+        optimal_matrix, optimal_df = create_matrices(script_id_optimal,
+                                                     1,
+                                                     entity_properties, 0)
 
-    # Evaluation
-    # Which rows and columns are matching and non-matching
-    matching_rows = eval_df.index.intersection(optimal_df.index)
-    matching_columns = eval_df.columns.intersection(optimal_df.columns)
-    non_matching_rows = eval_df.index.difference(optimal_df.index)
-    non_matching_columns = eval_df.columns.difference(optimal_df.columns)
+        # Evaluation
+        # Which rows and columns are matching and non-matching
+        matching_rows = eval_df.index.intersection(optimal_df.index)
+        matching_columns = eval_df.columns.intersection(optimal_df.columns)
+        non_matching_rows = eval_df.index.difference(optimal_df.index)
+        non_matching_columns = eval_df.columns.difference(optimal_df.columns)
 
-    if evaluating_framework == 1:  # Fill up the dimensionalities
+        if evaluating_framework == 1:  # Fill up the dimensionalities
+            print('Fill up dimensionalities evaluation')
+            # Clean the evaluated space from dimensions which are not in the optimal space
+            # If there are dimensions in the optimal space which are not in the evaluated space fill the ones in the evaluated space with zero
+            filtered_eval_df = eval_df.loc[eval_df.index.intersection(optimal_df.index), eval_df.columns.intersection(optimal_df.columns)]
+            aligned_evaluation_df = filtered_eval_df.reindex(index=optimal_df.index, columns=optimal_df.columns, fill_value=0)
+            # print('ALIGNED EVALUATION', aligned_evaluation_df)
 
-        # Clean the evaluated space from dimensions which are not in the optimal space
-        # If there are dimensions in the optimal space which are not in the evaluated space fill the ones in the evaluated space with zero
-        filtered_eval_df = eval_df.loc[eval_df.index.intersection(optimal_df.index), eval_df.columns.intersection(optimal_df.columns)]
-        aligned_evaluation_df = filtered_eval_df.reindex(index=optimal_df.index, columns=optimal_df.columns, fill_value=0)
-        print('ALIGNED EVALUATION', aligned_evaluation_df)
-
-        # Compute the cosine similarity between rows and then average the values in one
-        similarity_df = pd.DataFrame(
-            cosine_similarity(optimal_df.to_numpy(), aligned_evaluation_df.to_numpy()),
-            index=optimal_df.index, columns=aligned_evaluation_df.index
-        )
-        print('ROW-WISE COSINE SIMILARITIES:\n', similarity_df)
-        print('Average similarity rows', similarity_df.values.mean())
-
-        # Compute cosine similarity between columns and then average the values in one
-        column_similarity_df = pd.DataFrame(
-            cosine_similarity(optimal_df.T.to_numpy(), aligned_evaluation_df.T.to_numpy()),
-            index=optimal_df.columns, columns=aligned_evaluation_df.columns
-        )
-        print('COLUMN-WISE COSINE SIMILARITIES:\n', column_similarity_df)
-        print('AVERAGE COLUMN SIMILARITY:', column_similarity_df.values.mean())
-
-    elif evaluating_framework == 2:  # Only match dimensions
-
-        # Extract from both the training and the evaluated file just the matching dimensions (intersections)
-        new_optimal_df = optimal_df.loc[matching_rows, matching_columns]
-        new_eval_df = eval_df.loc[matching_rows, matching_columns]
-
-        # Compute the cosine similarity between rows
-        row_similarity_df = pd.DataFrame(
-            cosine_similarity(new_optimal_df.to_numpy(), new_eval_df.to_numpy()),
-            index=new_optimal_df.index,
-            columns=new_eval_df.index
-        )
-        print('ROW-WISE COSINE SIMILARITIES:\n', row_similarity_df)
-        print('Average similarity (rows):', row_similarity_df.values.mean())
-
-        # And between columns, transposing the matrix
-        column_similarity_df = pd.DataFrame(
-            cosine_similarity(new_optimal_df.T.to_numpy(), new_eval_df.T.to_numpy()),
-            index=new_optimal_df.columns,
-            columns=new_eval_df.columns
-        )
-        print('COLUMN-WISE COSINE SIMILARITIES:\n', column_similarity_df)
-        print('Average similarity (columns):', column_similarity_df.values.mean())
-
-    elif evaluating_framework == 3:  # RSA-based evaluation
-
-        if not non_matching_rows.empty:
-            print(f"Warning: The following rows in eval_df are not in optimal_df and will be ignored: {non_matching_rows.tolist()}")
-        if not non_matching_columns.empty:
-            print(f"Warning: The following columns in eval_df are not in optimal_df and will be ignored: {non_matching_columns.tolist()}")
-
-        # Keeps only the values in the evaluated df which are present in the optimal df, aligns the indexes and fill missing values with NaN
-        aligned_evaluation_df = eval_df.loc[matching_rows, matching_columns].reindex(index=optimal_df.index, columns=optimal_df.columns, fill_value=np.nan)
-        print('ALIGNED EVAL 1', aligned_evaluation_df)
-
-        # For NaN values in one column, fills the value with the mean of the values in the same column
-        aligned_evaluation_df = aligned_evaluation_df.apply(lambda col: col.fillna(col.mean()), axis=0)
-        print('ALIGNED EVAL 2', aligned_evaluation_df)
-
-        # Fill NaN columns with the average mean of the values in the df
-        for column in aligned_evaluation_df.columns:
-            aligned_evaluation_df[column] = aligned_evaluation_df.apply(
-                lambda row: row.drop(column).mean() if pd.isna(row[column]) else row[column],
-                axis=1
+            # Compute the cosine similarity between rows and then average the values in one
+            row_similarity_df = pd.DataFrame(
+                cosine_similarity(optimal_df.to_numpy(), aligned_evaluation_df.to_numpy()),
+                index=optimal_df.index, columns=aligned_evaluation_df.index
             )
-        print('ALIGNED EVAL 3', aligned_evaluation_df)
+            # print('ROW-WISE COSINE SIMILARITIES:\n', row_similarity_df)
+            # print('AVERAGE ROW SIMILARITY', row_similarity_df.values.mean())
+            # print((optimal_df.to_numpy() == aligned_evaluation_df.to_numpy()).all()
+            row_diagonal_values = row_similarity_df.to_numpy().diagonal()
+            row_diagonal_series = pd.Series(row_diagonal_values, index=row_similarity_df.index)
+            # print(row_diagonal_series)
+            print('MEAN ROW SIMILARITY', "{:.3f}".format(row_diagonal_series.values.mean()))
+            row_5_highest = row_diagonal_series.nlargest(5)
+            print('Most similar rows:', row_5_highest)
 
-        if aligned_evaluation_df.isna().sum().sum() > 0:
-            print("There are still NaN values in the aligned evaluation DataFrame.")
-        if optimal_df.isna().sum().sum() > 0:
-            print("There are still NaN values in the optimal DataFrame.")
+            # Compute cosine similarity between columns and then average the values in one
+            column_similarity_df = pd.DataFrame(
+                cosine_similarity(optimal_df.T.to_numpy(), aligned_evaluation_df.T.to_numpy()),
+                index=optimal_df.columns, columns=aligned_evaluation_df.columns
+            )
+            # print('COLUMN-WISE COSINE SIMILARITIES:\n', column_similarity_df)
+            # print('AVERAGE COLUMN SIMILARITY:', column_similarity_df.values.mean())
+            col_diagonal_values = column_similarity_df.to_numpy().diagonal()
+            col_diagonal_series = pd.Series(col_diagonal_values, index=column_similarity_df.index)
+            # print(col_diagonal_series
+            print('MEAN COL SIMILARITY', "{:.3f}".format(col_diagonal_series.values.mean()))
+            col_5_highest = col_diagonal_series.nlargest(5)
+            print('Most similar cols:', col_5_highest)
 
-        print('ENSURING', optimal_df.dtypes)  # Ensure all values are numeric
-        print('ENSURING', aligned_evaluation_df.dtypes)
+        elif evaluating_framework == 2:  # Only match dimensions
 
-        # Compute RSA Score
-        optimal_dissimilarity = squareform(pdist(optimal_df, metric='cosine'))
-        evaluation_dissimilarity = squareform(pdist(aligned_evaluation_df, metric='cosine'))
-        rsa_score, _ = spearmanr(optimal_dissimilarity.flatten(), evaluation_dissimilarity.flatten())
+            print('Retraction evaluation')
+            # Extract from both the training and the evaluated file just the matching dimensions (intersections)
+            new_optimal_df = optimal_df.loc[matching_rows, matching_columns]
+            new_eval_df = eval_df.loc[matching_rows, matching_columns]
 
-        print("RSA Score:", rsa_score)
+            # Compute the cosine similarity between rows
+            row_similarity_df = pd.DataFrame(
+                cosine_similarity(new_optimal_df.to_numpy(), new_eval_df.to_numpy()),
+                index=new_optimal_df.index,
+                columns=new_eval_df.index
+            )
+            row_diagonal_values = row_similarity_df.to_numpy().diagonal()
+            row_diagonal_series = pd.Series(row_diagonal_values, index=row_similarity_df.index)
+            # print(row_diagonal_series)
+            print('Average similarity diagonal cells - row', row_diagonal_series.values.mean())
+            row_5_highest = row_diagonal_series.nlargest(5)
+            print('Most similar rows:', row_5_highest)
+
+            # And between columns, transposing the matrix
+            column_similarity_df = pd.DataFrame(
+                cosine_similarity(new_optimal_df.T.to_numpy(), new_eval_df.T.to_numpy()),
+                index=new_optimal_df.columns,
+                columns=new_eval_df.columns
+            )
+            col_diagonal_values = column_similarity_df.to_numpy().diagonal()
+            col_diagonal_series = pd.Series(col_diagonal_values, index=column_similarity_df.index)
+            # print(col_diagonal_series
+            print('Average similarity diagonal cells - cols', col_diagonal_series.values.mean())
+            col_5_highest = col_diagonal_series.nlargest(5)
+            print('Most similar rows:', col_5_highest)
+
+        elif evaluating_framework == 3:  # RSA-based evaluation
+            print('RSA evaluation')
+            if not non_matching_rows.empty:
+                print(f"Warning: The following rows in eval_df are not in optimal_df and will be ignored: {non_matching_rows.tolist()}")
+            if not non_matching_columns.empty:
+                print(f"Warning: The following columns in eval_df are not in optimal_df and will be ignored: {non_matching_columns.tolist()}")
+
+            # Keeps only the values in the evaluated df which are present in the optimal df, aligns the indexes and fill missing values with NaN
+            aligned_evaluation_df = eval_df.loc[matching_rows, matching_columns].reindex(index=optimal_df.index, columns=optimal_df.columns, fill_value=np.nan)
+            # print('ALIGNED EVAL 1', aligned_evaluation_df)
+
+            # For NaN values in one column, fills the value with the mean of the values in the same column
+            aligned_evaluation_df = aligned_evaluation_df.apply(lambda col: col.fillna(col.mean()), axis=0)
+            # print('ALIGNED EVAL 2', aligned_evaluation_df)
+
+            # Fill NaN columns with the average mean of the values in the df
+            for column in aligned_evaluation_df.columns:
+                aligned_evaluation_df[column] = aligned_evaluation_df.apply(
+                    lambda row: row.drop(column).mean() if pd.isna(row[column]) else row[column],
+                    axis=1
+                )
+            # print('ALIGNED EVAL 3', aligned_evaluation_df)
+
+            if aligned_evaluation_df.isna().sum().sum() > 0:
+                print("There are still NaN values in the aligned evaluation DataFrame.")
+            if optimal_df.isna().sum().sum() > 0:
+                print("There are still NaN values in the optimal DataFrame.")
+
+            # print('ENSURING', optimal_df.dtypes)  # Ensure all values are numeric
+            # print('ENSURING', aligned_evaluation_df.dtypes)
+
+            # Compute RSA Score
+            optimal_dissimilarity = squareform(pdist(optimal_df, metric='cosine'))
+            evaluation_dissimilarity = squareform(pdist(aligned_evaluation_df, metric='cosine'))
+            rsa_score, _ = spearmanr(optimal_dissimilarity.flatten(), evaluation_dissimilarity.flatten())
+
+            print("RSA Score:", rsa_score)
