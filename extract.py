0c03a250bc2f94791047c833f9176992a1f57b04
minimalparts <aurelie.herbelot@cantab.net>
2025-03-24 18:28:51 +0100

    Added extraction of region graph objects

--- extract.py
+++ extract.py
@@ -1,5 +1,6 @@
 import io
 import json
+import ijson
 import requests
 import zipfile
 from tqdm import tqdm
@@ -79,9 +80,42 @@ def extract_qa_for_training(fpath, activity=None):
     print(f">> DATA: VISUAL GENOME EXTRACTING: extracted {c} QA pairs.\n")
 
 
+def extract_object_descriptions_for_training(fpath, activity=None):
+    if activity not in ['dsc']:
+        print(f">> DATA: VISUAL GENOME EXTRACTION: ERROR: activity type {activity} not supported.")
+        return 0
+
+    print(f">> DATA: VISUAL GENOME EXTRACTING: {fpath} for training ({activity})")
+    Path(activity).mkdir(parents=True, exist_ok=True)
+
+    content = {}
+    with zipfile.ZipFile(fpath) as zf:
+        with io.TextIOWrapper(zf.open(fpath.replace('.zip','')), encoding="utf-8") as f:
+            for record in ijson.items(f, "item"):
+                for region in record["regions"]:
+                    reg_id = region["region_id"]
+                    objects = region["objects"]
+                    phrase = region["phrase"]
+                    if len(objects) > 0:
+                        obj_ids = [obj["object_id"] for obj in objects]
+                        print(reg_id, phrase, obj_ids)
+                        content[reg_id] = {"description": phrase, "objects": obj_ids}
+
+    with open(join(activity,fpath.replace('.zip','.'+activity)), 'w', encoding="utf-8") as f:
+        c = 0
+        for k,v in content.items():
+            f.write(f"<a type=DSC idx={k}>\n")
+            f.write(f"<u speaker=HUM>{v['objects']}</u>\n<u speaker=BOT>{v['description']}</u>\n")
+            f.write(f"</a>\n")
+            c+=1
+    print(f">> DATA: VISUAL GENOME EXTRACTING: extracted {c} regions.\n")
+
 qapath = 'question_answers.json.zip'
 rdpath = 'region_descriptions.json.zip'
+rgpath = 'region_graphs.json.zip'
 download_vg(qapath)
 download_vg(rdpath)
+download_vg(rgpath)
 extract_qa_for_training(qapath,'skt')
 extract_rd_for_training(rdpath,'obs')
+extract_object_descriptions_for_training(rgpath, 'dsc')
