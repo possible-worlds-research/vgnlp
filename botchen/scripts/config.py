519ad31110b7621a12c4548b6e8de9cc9e8acaf3
minimalparts <aurelie.herbelot@cantab.net>
2025-06-26 11:45:43 +0200

    Added code to automatically select n random situation ids.

--- botchen/scripts/config.py
+++ botchen/scripts/config.py
@@ -1,57 +1,48 @@
+from os.path import dirname, realpath, join
+from random import shuffle
 
 '''
 BEGINNING DYNAMICAL PARAMETERS WHICH THE USER CAN CHANGE
 '''
 
-# These are the mapping ids from the ideallangueg/visualGenome to the new ids
-ids= [
-        (2317468, 1), # person
-        (2396154, 2), # animal
-        (186, 3), # food
-        (2410753, 4), # desktop 
-        (1, 5), # street
-        (4, 6), # bedroom
+configs = {}
 
-        (2412620, 7), # person
-        (2412211, 8), # animal
-        (713137, 9), # food
-        (3, 10), # desktop
-        (2357183, 11), # street
-        (9, 12), # bedroom/room
+def mk_idx_list():
+    parent_dir = dirname(dirname(realpath(__file__)))
+    ideallanguage = join(parent_dir, "data", "ideallanguage.txt")
 
-        (2343232, 13), # person
-        (2406947, 14), # animal
-        (2343284, 15), # food
-        (1515, 16), # desktop
-        (2343307, 17), # street
-        (2361685, 18) # bedroom/room
-        ]
+    ids = []
+    with open(ideallanguage, 'r', encoding='utf-8') as fin:
+        for l in fin:
+            if l.startswith('<situation id='):
+                l = l.rstrip('\n').strip()
+                idx = int(l.replace('<situation id=','')[:-1])
+                ids.append(idx)
 
-substitution_terms_list = [
-        'car','jacket','shirt', 'man','woman', 'tree','road', 'bicycle', 
-        'gym_shoe','table', 'curtain', 'sofa', 'chair', 'picture', 'teddy', 
-        'desk','jean','room', 'ceiling', 'shelf', 'picture', 'monitor', 'bottle', 
-        'sunset', 'mouse', 'part','cup', 'egg', 'muffin', 'plate', 'tomato', 
-        'sauce', 'tea', 'spoon','mouth', 'watch','giraffe', 'branch', 
-        'neck', 'eye','basket', 'ginger', 'vegetable', 'bowl', 'cheese', 
-        'chopstick','grass', 'elephant', 'trunk','suit', 'belt', 'hair', 'earring'
-        ]
+    n = 2000
+    print(f"Found {len(ids)} situations. Picking a random {n}...")
+    shuffle(ids)
+    ids = ids[:n]
+    for i, idx in enumerate(ids):
+        ids[i] = (idx,i+1)
+    return ids
 
-extend_corpus = True
-permutation_flag = True  # This applies the permutations and writes the files 
-training_and_test_sets = True 
+configs['ids'] = mk_idx_list()
+substitution_term_list = []
+configs['extend_corpus'] = False
+configs['permutation_flag'] = False  # This applies the permutations and writes the files 
+configs['training_and_test_sets'] = True 
 
-write_all_files = False # This makes it write files of augmented and original
+configs['write_all_files'] = True # This makes it write files of augmented and original
 
-limited = False
-limited_max_utterances = 5 # These make the situation be of x utterances
-test_mode = False
-test_max_situations = 3 # These make the x situations from which we extract
+configs['limited'] = False
+configs['limited_max_utterances'] = 5 # These make the situation be of x utterances
+configs['test_mode'] = False
+configs['test_max_situations'] = 3 # These make the x situations from which we extract
 
-if extend_corpus:
-    min_referent_overlap_ratio=0.7 # FOCUSED ON REFERENT SITUATION Minimum proportion of referent entities that must appear in a target situation (i.e. we apply this to referent situations, e.g. *1 if the referent situation is as such)
-    min_target_overlap_ratio=0.1 # FOCUSED ON TARGET SITUATION  Minimum proportion of target entities that must match referent entities (i.e. we apply this to all the *10 situations which we are finding similar to a referent situation *1)
-    min_content_length=1000 # Minimum number of characters in a situation's content
-    max_content_length=200000 # Maximum number of characters in a situation's content
-    max_per_referent=10 # Maximum number of similar situations to extract per referent situation (e.g. we take *10* situations similar to situation 1, *10* to situation 2)
-    train_split_ratio=0.7 # Percentage of training and testing sets
+configs['min_referent_overlap_ratio'] = 0.7 # FOCUSED ON REFERENT SITUATION Minimum proportion of referent entities that must appear in a target situation (i.e. we apply this to referent situations, e.g. *1 if the referent situation is as such)
+configs['min_target_overlap_ratio'] = 0.1 # FOCUSED ON TARGET SITUATION  Minimum proportion of target entities that must match referent entities (i.e. we apply this to all the *10 situations which we are finding similar to a referent situation *1)
+configs['min_content_length'] = 1000 # Minimum number of characters in a situation's content
+configs['max_content_length'] = 200000 # Maximum number of characters in a situation's content
+configs['max_per_referent'] = 10 # Maximum number of similar situations to extract per referent situation (e.g. we take *10* situations similar to situation 1, *10* to situation 2)
+configs['train_split_ratio'] = 0.7 # Percentage of training and testing sets
